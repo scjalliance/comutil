8c91e1d8d3bda3f43f3b56dd413384724409b91f
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2016-06-22 16:54:00 -0700

    Added IIDFromString to syscall
    This is the official function used for converting strings into GUIDs

--- syscall.go
+++ syscall.go
@@ -13,6 +13,7 @@ var (
 
 var (
 	procCoCreateInstanceEx, _ = modole32.FindProc("CoCreateInstanceEx")
+	procIIDFromString, _      = modole32.FindProc("IIDFromString")
 )
 
 // CreateInstanceEx supports remote creation of multiple interfaces within one
@@ -35,6 +36,31 @@ func CreateInstanceEx(clsid *ole.GUID, context uint, serverInfo *CoServerInfo, r
 		uintptr(unsafe.Pointer(serverInfo)),
 		uintptr(len(results)),
 		uintptr(unsafe.Pointer(_p0)))
+	if hr != 0 {
+		err = ole.NewError(hr)
+	}
+	return
+}
+
+// IIDFromString takes the given value and attempts to convert it into a valid
+// GUID. If it fails it returns an error. It does not provide any additional
+// validation, such as checking the Windows registry for its registration.
+//
+// It is safe to use this function to parse any GUID, not just COM interface
+// identifiers.
+//
+// MSDN: https://msdn.microsoft.com/library/ms687262
+// Raymond Chen: https://blogs.msdn.microsoft.com/oldnewthing/20151015-00/?p=91351
+func IIDFromString(value string) (iid *ole.GUID, err error) {
+	bvalue := ole.SysAllocStringLen(value)
+	if bvalue == nil {
+		return nil, ole.NewError(ole.E_OUTOFMEMORY)
+	}
+	defer ole.SysFreeString(bvalue)
+	iid = new(ole.GUID)
+	hr, _, _ := procIIDFromString.Call(
+		uintptr(unsafe.Pointer(bvalue)),
+		uintptr(unsafe.Pointer(iid)))
 	if hr != 0 {
 		err = ole.NewError(hr)
 	}
