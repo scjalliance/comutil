cad3c2f727471277420a77480e75b73a43dcf7cf
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2017-11-14 15:29:14 -0800

    Added variant and safe array conversion functions

--- comutil.go
+++ comutil.go
@@ -1,6 +1,7 @@
 package comutil
 
 import (
+	"fmt"
 	"unsafe"
 
 	"github.com/go-ole/go-ole"
@@ -97,4 +98,156 @@ func SafeArrayFromStringSlice(slice []string) *ole.SafeArray {
 		SafeArrayPutElement(array, int64(i), unsafe.Pointer(ole.SysAllocStringLen(v)))
 	}
 	return array
+}
+
+// VariantToValue attempts to convert the given variant to a native Go
+// representation.
+func VariantToValue(variant *ole.VARIANT) (value interface{}, err error) {
+	if array := variant.ToArray(); array != nil {
+		return ArrayToValue(array)
+	}
+	return variant.Value(), nil
+}
+
+// ArrayToValue attempts to convert the given safe array to a native Go
+// representation.
+func ArrayToValue(array *ole.SafeArrayConversion) (value interface{}, err error) {
+	vt, err := array.GetType()
+	if err != nil {
+		return
+	}
+
+	if ole.VT(vt) == ole.VT_VARIANT {
+		return VariantArrayToValues(array)
+	}
+
+	return SimpleArrayToValues(array)
+}
+
+// SimpleArrayToValues attempts to convert the given safe array of non-variant
+// members to a native Go representation.
+//
+// The returned value will be a slice of elements of appropriate type.
+//
+// Currently only arrays of bytes and integers are supported.
+func SimpleArrayToValues(array *ole.SafeArrayConversion) (value interface{}, err error) {
+	vt, elems, err := arrayDetails(array)
+	if err != nil {
+		return
+	}
+	if vt == ole.VT_VARIANT {
+		return nil, ErrVariantArray
+	}
+
+	switch vt {
+	case ole.VT_UI1:
+		out := make([]byte, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_I1:
+		out := make([]int8, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_UI2:
+		out := make([]uint16, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_I2:
+		out := make([]int16, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_UI4:
+		out := make([]uint32, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_I4:
+		out := make([]int32, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_UI8:
+		out := make([]uint64, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	case ole.VT_I8:
+		out := make([]int64, elems)
+		for i := int64(0); i < elems; i++ {
+			copyArrayElement(array.Array, i, unsafe.Pointer(&out[i]), &err)
+		}
+		value = out
+	default:
+		err = ErrUnsupportedArray
+	}
+	return
+}
+
+// VariantArrayToValues attempts to convert the given safe array of variant
+// members to a native Go representation.
+func VariantArrayToValues(array *ole.SafeArrayConversion) (values []interface{}, err error) {
+	vt, elems, err := arrayDetails(array)
+	if err != nil {
+		return
+	}
+	if vt != ole.VT_VARIANT {
+		return nil, ErrNonVariantArray
+	}
+
+	for i := int64(0); i < elems; i++ {
+		element := &ole.VARIANT{}
+		ole.VariantInit(element)
+		err = SafeArrayGetElement(array.Array, i, unsafe.Pointer(element))
+		if err != nil {
+			err = fmt.Errorf("unable to retrieve array element %d: %v", i, err)
+			ole.VariantClear(element)
+			return
+		}
+		value, valueErr := VariantToValue(element)
+		if valueErr != nil {
+			if err == nil {
+				err = fmt.Errorf("unable to interpret array element %d: %v", i, valueErr)
+			}
+		} else {
+			values = append(values, value)
+		}
+		ole.VariantClear(element)
+	}
+
+	return
+}
+
+func arrayDetails(array *ole.SafeArrayConversion) (vt ole.VT, elems int64, err error) {
+	_vt, err := array.GetType()
+	if err != nil {
+		return
+	}
+	vt = ole.VT(_vt)
+
+	dims, _ := SafeArrayGetDim(array.Array) // Error intentionally ignored
+	if dims != 1 {
+		err = ErrMultiDimArray
+		return
+	}
+
+	elems, err = array.TotalElements(0)
+	return
+}
+
+func copyArrayElement(from *ole.SafeArray, index int64, to unsafe.Pointer, err *error) {
+	e := SafeArrayGetElement(from, index, to)
+	if e != nil && *err == nil {
+		*err = fmt.Errorf("unable to retrieve array element %d: %v", index, e)
+	}
 }
--- constants.go
+++ constants.go
@@ -9,4 +9,20 @@ var (
 	// proper error code. Under normal circumstances with correctly written
 	// component object model servers this error should never occur.
 	ErrCreationFailed = errors.New("unable to create interface instance")
+
+	// ErrMultiDimArray is returned when a safe array contains more than one
+	// one dimension. Multi-dimensional array parsing is not currently supported.
+	ErrMultiDimArray = errors.New("attribute contains a multi-dimensional array of values")
+
+	// ErrUnsupportedArray is returned when members of a safe array members are
+	// supported for type conversion.
+	ErrUnsupportedArray = errors.New("unsupported safe array member type")
+
+	// ErrVariantArray is returned when members of a safe array members were not
+	// expected to variants but are.
+	ErrVariantArray = errors.New("attribute contains variant array members")
+
+	// ErrNonVariantArray is returned when members of a safe array members were
+	// expected to variants but are not.
+	ErrNonVariantArray = errors.New("attribute contains non-variant array members")
 )
