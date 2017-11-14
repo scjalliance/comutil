0de5c49832731839413630a31a2ff1be40366a20
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2017-11-14 12:16:48 -0800

    Adjusted case and punctuation of error messages to match Go conventions

--- constants.go
+++ constants.go
@@ -8,5 +8,5 @@ var (
 	// CoCreateInstanceEx but an HRESULT value of S_OK was returned instead of a
 	// proper error code. Under normal circumstances with correctly written
 	// component object model servers this error should never occur.
-	ErrCreationFailed = errors.New("Unable to create interface instance.")
+	ErrCreationFailed = errors.New("unable to create interface instance")
 )
